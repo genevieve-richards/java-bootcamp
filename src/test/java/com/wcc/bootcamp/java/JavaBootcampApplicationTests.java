0b376b0e85fb9cd6dbba58fb8bc32d0bc9db7b3a
Adriana Zencke Zimmermann <dricazenck@gmail.com>
2026-02-04 20:44:49 +0100

    first commit

---
+++ src/test/java/com/wcc/bootcamp/java/JavaBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcc.bootcamp.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
